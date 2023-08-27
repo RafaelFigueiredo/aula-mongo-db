7411657abb60a0a30b9fd9d97f78088cd35cb381
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2023-08-26 23:02:14 -0300

    any value as telemetry

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.post("/telemetry")
 def post_telemetry():
-    body = request.get_json()
-    print(body)
+    telemetry = request.get_json()
+
+    print(telemetry)
     return {
         'ok': True
     }
