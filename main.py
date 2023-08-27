adc0e0ecbba8e5f7d3afe332c44656d372c5b12b
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2023-08-26 22:54:06 -0300

    get body content and print

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.post("/telemetry")
 def post_telemetry():
-    
+    body = request.get_json()
+    print(body)
     return {
         'ok': True
     }
